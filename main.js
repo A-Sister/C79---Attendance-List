f4fce2844596801a6a758023e79a5a09ae591f99
A-Sister <92495146+A-Sister@users.noreply.github.com>
2022-02-02 17:52:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+student_name_array=[];
+
+function submit(){
+    var name1= document.getElementById("student1").value;
+    var name2= document.getElementById("student2").value;
+    var name3= document.getElementById("student3").value;
+    var name4= document.getElementById("student4").value;
+
+    student_name_array.push(name1);
+    student_name_array.push(name2);
+    student_name_array.push(name3);
+    student_name_array.push(name4);
+
+    console.log(student_name_array);
+    document.getElementById("students").innerHTML= student_name_array;
+    document.getElementById("submit_button").style.display= "none";
+    document.getElementById("sort").style.display="inline-block";
+}
+
+function sorting(){
+    student_name_array.sort();
+
+    console.log(student_name_array);
+    document.getElementById("students").innerHTML= student_name_array;
+}
